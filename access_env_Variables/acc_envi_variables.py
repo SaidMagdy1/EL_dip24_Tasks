db78f0f6bc67bd06a5391f2b234a131d6b1cfa3a
saidmagdy1 <mandopop978@gmail.com>
2024-06-13 11:13:11 +0300

    acc_var Task

--- access_env_Variables/acc_envi_variables.py
+++ access_env_Variables/acc_envi_variables.py
@@ -1,5 +1,14 @@
+#said magdy    G:A3
 
 import os
 
-environment_variables= os.getenv('MY_ENV_VAR')
-print(environment_variables)
+#list all the environment variables
+print("All environment variables: \n -------------------------------------")
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
+
+print(" -------------------------------------")
+
+#here how to add an environment variable
+os.environ['MY_NEW_ENV_VAR'] = 'My new environment variable'
+print(f"Value of MY_NEW_ENV_VAR: {os.getenv('MY_NEW_ENV_VAR')}")
