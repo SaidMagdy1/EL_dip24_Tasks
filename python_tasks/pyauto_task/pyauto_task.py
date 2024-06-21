d05a91c6cf974c54dccc78861c19f65053cf88bc
saidmagdy1 <mandopop978@gmail.com>
2024-06-21 16:07:45 +0300

    pyauto Task

---
+++ python_tasks/pyauto_task/pyauto_task.py
@@ -0,0 +1,26 @@
+#said magdy   G:A3
+import pyautogui
+from time import sleep
+
+#pyautogui.press('win')
+# sleep(3)
+# pyautogui.typewrite('vid')
+
+# while True :
+#     sleep(1)
+#     #pyautogui.moveTo(500,500,duration=1)  
+#     print(pyautogui.position())
+
+# location=None
+# while location is None :
+#     try:
+#         sleep(4)
+#         location = pyautogui.locateOnScreen('pic.png') 
+        
+#         print(location)
+#        # pyautogui.moveTo(location.left+20,location.top+10)   
+#     except pyautogui.ImageNotFoundException :
+#         print("image Not found : ")
+
+# pyautogui.moveTo(location.left+20,location.top+10)        
+# print("while ended !")
