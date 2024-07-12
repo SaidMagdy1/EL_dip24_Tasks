94c639aeb096bf4ad443479739dd6df0a556eb41
saidmagdy1 <mandopop978@gmail.com>
2024-07-12 15:51:43 +0300

    Cmake Test

---
+++ Cpp_tasks/cmake_test/build/CMakeFiles/myExc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myExc.dir/main.cpp.o"
+  "CMakeFiles/myExc.dir/main.cpp.o.d"
+  "myExc"
+  "myExc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myExc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Cpp_tasks/cmake_test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+
+
+int main()
+{
+std::string name = "said";
+std::cout <<"here is my name :"<<name <<std::endl;
+
+return 0;
+}
