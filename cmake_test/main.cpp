755e4c24cc8000fedf8723090fe8803b5d75dd4d
saidmagdy1 <mandopop978@gmail.com>
2024-07-17 21:18:05 +0300

    cmake dir

--- cmake_test/main.cpp
+++ cmake_test/main.cpp
@@ -2,13 +2,16 @@
 #include <string.h>
 #include "calc.hpp"
 #include "base.hpp"
-
+#include <vector>
 
 int main()
 {
+    std::vector<int>nums = {1,2,3,4,5,6};
 std::string name = "said";
 std::cout <<"here is my name :"<<name <<" ** -> sum: "<<sum(1.3,1.2)<<std::endl;
 base_INIT();
+
+std::cout <<"here is my name :"<<  nums.size() <<std::endl;
 
 return 0;
 }
