d606d63feb47f2267c5ab2e8c9ea339c3b2718c2
saidmagdy1 <mandopop978@gmail.com>
2024-09-18 14:41:39 +0300

    polymorph update

--- Cpp_tasks/polymorphizm.cpp
+++ Cpp_tasks/polymorphizm.cpp
@@ -4,7 +4,7 @@ class Base {
 public:
 int a;
 std::string name;
-virtual void greeting(){
+virtual void greeting() const{
     std::cout << "Hello from Base Class "<< std::endl;
 }
 
@@ -14,9 +14,11 @@ Base()
 }
 Base(int a)
 {
-    std::cout << "Base Constructor "<< a << std::endl;
+    std::cout << "Base Parm Constructor "<< a << std::endl;
 }
-~Base(){
+
+//whenever use virual fun U should use virtual destructor as well .
+virtual ~Base(){
     std::cout << "Base Destructor "<< std::endl;
 }
 };
@@ -27,14 +29,18 @@ Base(int a)
 class Child : public Base {
 public:
 using Base::Base;     // make sure to reach all the Base constructors 
-virtual void greeting(){
+virtual void greeting()const override{    //override Keyward will make sure its overriding not a redefinition
     std::cout << "Hello from Child Class "<< std::endl;
 }
 Child()
 {
     std::cout << "Child Constructor " << std::endl;
 }
-~Child(){
+Child(int a):Base{a}
+{
+    std::cout << "Child Parm Constructor " << a <<std::endl;
+}
+virtual ~Child(){
     std::cout << "Child Destructor "<< std::endl;
 }
 };
@@ -44,9 +50,20 @@ int main(){
 //dynamic binding (late binding)
     Base *ptr=new Base();
     Child g(5);  //Child doesnt have parm constructor but Base has and we used the using Class::Class
+std::cout << "-----------------------------------" <<std::endl;
     ptr->greeting();
     ptr=&g;
     ptr->greeting();
+std::cout << "-----------------------------------" <<std::endl;
+    Base obj{10};
+    Base &ref = obj;
+    ref.greeting();
+std::cout << "-----------------------------------" <<std::endl;
+    Child obj2{20};
+    Base &ref2=obj2;
+    ref2.greeting();
+std::cout << "-----------------------------------" <<std::endl;
+
 
     return 0;
 }
