16679ded3042fe7c6b231494c8d4397ec30f595f
saidmagdy1 <mandopop978@gmail.com>
2025-02-27 13:54:44 +0200

    smart_ptr update

--- Cpp_tasks/polymorphizm.cpp
+++ Cpp_tasks/polymorphizm.cpp
@@ -26,42 +26,42 @@ virtual ~Base(){
 
 
 
-class Child : public Base {
+class Derived : public Base {
 public:
-using Base::Base;     // make sure to reach all the Base constructors 
+//using Base::Base;     // make sure to reach all the Base constructors 
 virtual void greeting()const override{    //override Keyward will make sure its overriding not a redefinition
-    std::cout << "Hello from Child Class "<< std::endl;
+    std::cout << "Hello from Derived Class "<< std::endl;
 }
-Child()
+Derived()
 {
-    std::cout << "Child Constructor " << std::endl;
+    std::cout << "Derived Constructor " << std::endl;
 }
-Child(int a):Base{a}
+Derived(int a):Base{a}
 {
-    std::cout << "Child Parm Constructor " << a <<std::endl;
+    std::cout << "Derived Parm Constructor " << a <<std::endl;
 }
-virtual ~Child(){
-    std::cout << "Child Destructor "<< std::endl;
+virtual ~Derived(){
+    std::cout << "Derived Destructor "<< std::endl;
 }
 };
 
 
 int main(){
 //dynamic binding (late binding)
-    Base *ptr=new Base();
-    Child g(5);  //Child doesnt have parm constructor but Base has and we used the using Class::Class
-std::cout << "-----------------------------------" <<std::endl;
+    Base *ptr = new Base();
+    Derived g(5);  //Derived doesnt have parm constructor but Base has and we used the using Class::Class
+std::cout << "-------------------(1)----------------" <<std::endl;
     ptr->greeting();
-    ptr=&g;
+    ptr=&g;                         
     ptr->greeting();
-std::cout << "-----------------------------------" <<std::endl;
+std::cout << "-----------------(2)------------------" <<std::endl;
     Base obj{10};
     Base &ref = obj;
     ref.greeting();
-std::cout << "-----------------------------------" <<std::endl;
-    Child obj2{20};
+std::cout << "------------------(3)-----------------" <<std::endl;
+    Derived obj2{20};
     Base &ref2=obj2;
     ref2.greeting();
-std::cout << "-----------------------------------" <<std::endl;
+std::cout << "-------------------(4)----------------" <<std::endl;
     return 0;
-}
+} 
--- Cpp_tasks/smart_ptr.cpp
+++ Cpp_tasks/smart_ptr.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <memory>
+#include <vector>
 
 int main(){
 //unique_pointer
-    std::unique_ptr<int>p=std::make_unique<int>(100);
-    std::unique_ptr<int>p2=std::make_unique<int>(10);
+    std::unique_ptr<int>p = std::make_unique<int>(100);
+    std::unique_ptr<int>p2 = std::make_unique<int>(10);
+    //p=p2; //Error unique cant be assigned
     p = std::move(p2);
+    std::vector<std::unique_ptr<int>>vec;
+    //vec.push_back(p); //Error unique cant be copied
 
     std::cout <<"the value is : "<< *p<<std::endl;
     std::cout <<"the Raw ptr is : "<< p.get()<<std::endl;
@@ -14,12 +18,14 @@ int main(){
     std::shared_ptr<int>sp=std::make_shared<int>(100);
     std::shared_ptr<int>sp2=std::make_shared<int>(10);
     //sp = std::move(sp2);
-    sp = sp2 ; // it works cuz it is shared
+    sp = sp2 ; // it works cuz it is a shared
 
     std::cout <<"the value is : "<< *sp<<std::endl;
     std::cout <<"the Count is : "<< sp.use_count()<<std::endl;
     sp2.reset();
     std::cout <<"the Count is : "<< sp.use_count()<<std::endl;
+    std::cout <<"the Count is : "<< sp2.use_count()<<std::endl;  //sp2 released so its 0
+
 
 
     return 0;
